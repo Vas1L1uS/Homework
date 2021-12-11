32ed63b26437d3297ab50300616ff82d69b2e01c
Vas1L1uS <95376525+Vas1l1uS@users.noreply.github.com>
2021-12-11 22:08:55 +0300

    Homework
    Homework

---
+++ Homework/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace Homework1
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("world");
+            Console.Write("!!!");
+
+            ReadKey();
+        }
+    }
+}
